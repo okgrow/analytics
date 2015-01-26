88e45389d78b81c8d19f0f1269a7ad31faef21f2
Tony Semana <tony.semana@semanteks.com>
2015-01-26 10:14:18 -0500

    Remove assumption of meteor packages
    - check for Router and Meteor.user around default tracking

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'okgrow:analytics',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'simple analytics integration for meteor',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
@@ -12,18 +12,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
-  api.addFiles([
-    'lib/server/config/browser-policy.js'
-  ], 'server');
+  api.use(['browser-policy']);
+  api.addFiles('lib/browser-policy.js', 'server');
   api.addFiles([
     'lib/config.js',
     'lib/analytics.min.js',
     'lib/meteor-analytics.js'
   ], 'client');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('okgrow:analytics');
-  // api.addFiles('okgrow:analytics-tests.js');
 });

32b410aeb88822a95190821a7e8cad7f2809fe37
Tony Semana <tony.semana@semanteks.com>
2015-03-13 13:29:33 -0400

    Bump version #
    This fixes #6, and fixes #13

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'okgrow:analytics',
-  version: '0.2.0',
+  version: '0.2.1',
   // Brief, one-line summary of the package.
   summary: 'Complete Google Analytics, Mixpanel, KISSmetrics (and more) integration for Meteor',
   // URL to the Git repository containing the source code for this package.

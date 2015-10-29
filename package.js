04cd95743fe75d2b6f44323c2856945c41a34baa
Paul Dowman <paul@pauldowman.com>
2015-10-28 21:40:17 -0400

    Bump version to 1.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'okgrow:analytics',
-  version: '0.4.3',
+  version: '1.0.0',
   summary: 'Complete Google Analytics, Mixpanel, KISSmetrics (and more) integration for Meteor',
   git: 'https://github.com/okgrow/analytics',
   documentation: 'README.md'

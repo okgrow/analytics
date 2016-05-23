1129e6eb7818d5b635b1529be89afe75ef1f09ba
rgould <rwgould@gmail.com>
2016-05-23 14:25:02 -0700

    Revert "remove useless try catch"
    This reverts commit a49d76a45602a52caf4dcf28f8d55f764e70ecd2.

--- client/meteor-analytics.js
+++ client/meteor-analytics.js
@@ -5,166 +5,155 @@
 //
 // TODO consider whether we should export something like this, maybe provide
 // our own api instead of just using analytics.js' api
-var trackEventWhenReady = function() {
-    var _args = arguments;
-    analytics.ready(function() {
-        analytics.track.apply(this, _args);
-    });
+var trackEventWhenReady = function () {
+  var _args = arguments;
+  analytics.ready(function () {analytics.track.apply(this, _args);});
 };
 
-var trackPageWhenReady = function() {
-    var _args = arguments;
-    analytics.ready(function() {
-        analytics.page.apply(this, _args);
-    });
+var trackPageWhenReady = function () {
+  var _args = arguments;
+  analytics.ready(function () {analytics.page.apply(this, _args);});
 };
 
-var identifyWhenReady = function() {
-    var _args = arguments;
-    analytics.ready(function() {
-        analytics.identify.apply(this, _args);
-    });
+var identifyWhenReady = function () {
+  var _args = arguments;
+  analytics.ready(function () {analytics.identify.apply(this, _args);});
 };
 
 /*
- * getUserEmail()
- * Figure out the user's correct email address. This helps the differing keys
- * in the database when using oAuth login.
- */
-getUserEmail = function() {
-    if (Meteor.userId()) {
-        var user = AnalyticsUsers.findOne({
-            _id: Meteor.userId()
-        }, {
-            fields: {
-                emails: 1,
-                "services.facebook.email": 1,
-                "services.google.email": 1,
-                "services.github.email": 1
-            }
-        });
-        if (user && user.emails) {
-            if (user.emails[0]) {
-                return user.emails[0].address;
-            } else {
-                return null;
-            }
-        } else if (user && user.services) {
-            var services = user.services;
-            if (services.facebook) {
-                return services.facebook.email;
-            } else if (services.github) {
-                return services.github.email;
-            } else if (services.google) {
-                return services.google.email;
-            } else {
-                return null;
-            }
-        }
+* getUserEmail()
+* Figure out the user's correct email address. This helps the differing keys
+* in the database when using oAuth login.
+*/
+getUserEmail = function(){
+  if (Meteor.userId()) {
+    var user = AnalyticsUsers.findOne({_id: Meteor.userId()}, {
+      fields: {
+        emails: 1,
+        "services.facebook.email": 1,
+        "services.google.email": 1,
+        "services.github.email": 1
+      }
+    });
+    if ( user && user.emails ) {
+      if (user.emails[0]) {
+        return user.emails[0].address;
+      } else {
+        return null;
+      }
+    } else if ( user && user.services ) {
+      var services = user.services;
+      if ( services.facebook ) {
+        return services.facebook.email;
+      } else if ( services.github ) {
+        return services.github.email;
+      } else if ( services.google ) {
+        return services.google.email;
+      } else {
+        return null;
+      }
     }
+  }
 };
 
 var initialized;
-var trackLogins = function() {
-    // don't run the first time, but we need to access Meteor.userId()
-    // so that it's reactive
-    Meteor.userId();
-    if (initialized) {
-        // when Meteor.userId() changes this will run
-        if (Meteor.userId()) {
-            // TODO I think it's not guaranteed that userEmail has been set because
-            // the 'analyticsusers' publication might not be ready yet.
-            identifyWhenReady(Meteor.userId(), {
-                email: userEmail
-            });
-            trackEventWhenReady("Signed in");
-        } else {
-            trackEventWhenReady("Signed out");
-        }
+var trackLogins = function () {
+  // don't run the first time, but we need to access Meteor.userId()
+  // so that it's reactive
+  Meteor.userId();
+  if (initialized) {
+    // when Meteor.userId() changes this will run
+    if (Meteor.userId()) {
+      // TODO I think it's not guaranteed that userEmail has been set because
+      // the 'analyticsusers' publication might not be ready yet.
+      identifyWhenReady(Meteor.userId(), {email: userEmail});
+      trackEventWhenReady("Signed in");
+    } else {
+      trackEventWhenReady("Signed out");
     }
-    initialized = true;
+  }
+  initialized = true;
 };
 
 
 var _IronRouter = (Package['iron:router'] && Package['iron:router'].Router);
 var _FlowRouter = (Package['kadira:flow-router'] && Package['kadira:flow-router'].FlowRouter) ||
-    (Package['meteorhacks:flow-router'] && Package['meteorhacks:flow-router'].FlowRouter) ||
-    (Package['kadira:flow-router-ssr'] && Package['kadira:flow-router-ssr'].FlowRouter) ||
-    (Package['meteorhacks:flow-router-ssr'] && Package['meteorhacks:flow-router-ssr'].FlowRouter);
+                  (Package['meteorhacks:flow-router'] && Package['meteorhacks:flow-router'].FlowRouter) ||
+                  (Package['kadira:flow-router-ssr'] && Package['kadira:flow-router-ssr'].FlowRouter) ||
+                  (Package['meteorhacks:flow-router-ssr'] && Package['meteorhacks:flow-router-ssr'].FlowRouter);
 
-initIronRouter = function() {
-    if (_IronRouter) {
-        _IronRouter.onRun(function() {
-            var router = this;
-            Tracker.afterFlush(function() {
-                trackPageWhenReady(router.route.getName());
-            });
-            this.next();
-        });
-    }
+initIronRouter = function(){
+  if (_IronRouter) {
+    _IronRouter.onRun(function() {
+      var router = this;
+      Tracker.afterFlush(function () { trackPageWhenReady(router.route.getName()); });
+      this.next();
+    });
+  }
 }
 
-initFlowRouter = function() {
-    if (_FlowRouter) {
-        // something context & context.context don't exist, see: #93
-        _FlowRouter.triggers.enter([
-            function(context) {
-                var page = {};
+initFlowRouter = function(){
+  if (_FlowRouter) {
+    // something context & context.context don't exist, see: #93
+    _FlowRouter.triggers.enter([function(context){
+      var page = {};
 
-                if (context.path) {
-                    page.path = context.path;
-                }
-                if (context.context && context.context.title) {
-                    page.title = context.context.title;
-                }
+      if (context.path){
+        page.path = context.path;
+      }
+      if (context.context && context.context.title){
+        page.title = context.context.title;
+      }
 
-                page.url = window.location.origin + page.path;
+      page.url = window.location.origin + page.path;
 
-                if (context.route && context.route.name) {
-                    page.name = context.route.name;
-                } else {
-                    page.name = page.path;
-                }
-                if (context.context && context.context.querystring) {
-                    page.search = "?" + context.context.querystring;
-                } else {
-                    page.search = "";
-                }
-                if (_FlowRouter.lastRoutePath) {
-                    page.referrer = window.location.origin + _FlowRouter.lastRoutePath;
-                } else {
-                    page.referrer = document.referrer;
-                }
-                _FlowRouter.lastRoutePath = page.path;
+      if (context.route && context.route.name) {
+        page.name = context.route.name;
+      } else {
+        page.name = page.path;
+      }
+      if (context.context && context.context.querystring) {
+        page.search = "?" + context.context.querystring;
+      } else {
+        page.search = "";
+      }
+      if (_FlowRouter.lastRoutePath) {
+        page.referrer = window.location.origin + _FlowRouter.lastRoutePath;
+      } else {
+        page.referrer = document.referrer;
+      }
+      _FlowRouter.lastRoutePath = page.path;
 
-                trackPageWhenReady(page.name, page);
-            }
-        ]);
-    }
+      trackPageWhenReady(page.name, page);
+    }]);
+  }
 }
 
 
 var userEmail;
-Meteor.startup(function() {
-    if (Meteor.settings && Meteor.settings.public && Meteor.settings.public.analyticsSettings) {
-        var settings = Meteor.settings.public.analyticsSettings;
-        if (settings.autorun !== false) {
-            initFlowRouter();
-            initIronRouter();
-        }
-        analytics.initialize(settings);
-    } else {
-        console.log("Missing analyticsSettings in Meteor.settings.public");
+Meteor.startup(function () {
+  if (Meteor.settings && Meteor.settings.public && Meteor.settings.public.analyticsSettings) {
+    var settings = Meteor.settings.public.analyticsSettings;
+    if(settings.autorun !== false){
+      initFlowRouter();
+      initIronRouter();
     }
+    analytics.initialize(settings);
+  } else {
+    console.log("Missing analyticsSettings in Meteor.settings.public");
+  }
 
-    if (Package['accounts-base']) {
-        Tracker.autorun(function() {
-            userEmail = getUserEmail();
-        });
-        Tracker.autorun(trackLogins);
-    }
+  if (Package['accounts-base']) {
+    Tracker.autorun(function () {
+      userEmail = getUserEmail();
+    });
+    Tracker.autorun(trackLogins);
+  }
 });
 
 
 analytics = this.analytics;
-delete this.analytics;
+try {
+    delete this.analytics;
+} catch (e) {
+}

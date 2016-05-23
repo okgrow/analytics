6ccd40087da67a1fe79e3d2d7c09748a89a3e45b
rgould <rwgould@gmail.com>
2016-05-23 14:30:03 -0700

    Release v1.0.7 - Meteor 1.3 support

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'okgrow:analytics',
-  version: '1.0.6',
+  version: '1.0.7',
   summary: 'Complete Google Analytics, Mixpanel, KISSmetrics (and more) integration for Meteor',
   git: 'https://github.com/okgrow/analytics',
   documentation: 'README.md'

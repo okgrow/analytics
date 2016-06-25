337e2b0c9ff44ac2b7a156137a6815b8ec7449e3
Charles Nelson <charlesnelson28@gmail.com>
2016-06-24 20:02:21 -0400

    fix FlowRouter example for when analytics.js is blocked by adblocker

--- examples/flow-router/flow-router.js
+++ examples/flow-router/flow-router.js
@@ -27,29 +27,34 @@ if (Meteor.isClient) {
     }
   }); // without 'name', path is used for tracking
 
-
-
   Template.mainLayout.onCreated(function () {
     Meteor.subscribe('oauthInfo');
     var self = this;
-    self.currentIdentity = new ReactiveVar(analytics._user._getTraits().email || 'No Identity Set');
     self.log = new ReactiveVar([]);
-    analytics.on('page', function(event, properties, options){
+    self.currentIdentity = new ReactiveVar("No Identity Set");
+
+    // We don't want to register analytics if it has been blocked by an adblocker.
+    if (typeof analytics === "undefined") return;
+
+    self.currentIdentity.set(analytics._user._getTraits().email || "No Identity Set");
+
+    analytics.on('page', function(event, properties, options) {
       var latest = self.log.get();
       latest.push("Page: " + options.path);
       self.log.set(latest);
     });
-    analytics.on('identify', function(event, properties, options){
+
+    analytics.on('identify', function(event, properties, options) {
       var latest = self.log.get();
       latest.push("Identify: " + properties.email);
       self.log.set(latest);
       self.currentIdentity.set(properties.email);
     });
-    analytics.on('track', function(event, properties, options){
+
+    analytics.on('track', function(event, properties, options) {
       var latest = self.log.get();
       latest.push("Track: " + event);
       self.log.set(latest);
-
     });
   });
 
@@ -67,8 +72,8 @@ if (Meteor.isClient) {
         } else if (user.services.google) {
           message = "Signed in with Google as " + user.services.google.name + " (" + user.services.google.email + ")";
         } else {
-          message = "Not an oauth login"
-        };
+          message = "Not an oauth login";
+        }
         return message;
       }
     }

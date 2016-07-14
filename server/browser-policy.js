498df94aed2a1d489dedf5fd83032c86d2663618
Charles Nelson <charlesnelson28@gmail.com>
2016-07-14 13:33:06 -0400

    es6 style for browser-policy, added eslint global ignore

--- server/browser-policy.js
+++ server/browser-policy.js
@@ -1,5 +1,7 @@
+/* global Package */
+
 if (Package["browser-policy-common"]) {
-  var content = Package['browser-policy-common'].BrowserPolicy.content;
+  const content = Package["browser-policy-common"].BrowserPolicy.content;
   if (content) {
     content.allowOriginForAll("https://www.google.com/analytics/");
     content.allowOriginForAll("https://cdn.mxpnl.com");

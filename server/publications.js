f1cecde2116dce68734437bd1be7fac51c4d2b58
Charles Nelson <charlesnelson28@gmail.com>
2016-07-14 13:35:45 -0400

    es6 style conversion & style fix for publications.js

--- server/publications.js
+++ server/publications.js
@@ -2,20 +2,21 @@
 /* global Mongo */
 
 Meteor.publish(null, function() {
-  if(this.userId) {
-    var self = this;
-    var query = Meteor.users.find(
-      {_id: this.userId},
-      {fields: {
-                  emails: 1,
-                  'services.facebook.email': 1,
-                  'services.google.email': 1,
-                  'services.github.email': 1 }});
-
-    Mongo.Collection._publishCursor(query, self, 'analyticsusers');
+  if (this.userId) {
+    const self = this;
+    const query = Meteor.users
+      .find({
+        _id: this.userId,
+      }, {
+        fields: {
+          emails: 1,
+          "services.google.email": 1,
+          "services.github.email": 1,
+          "services.facebook.email": 1,
+        },
+      });
+    Mongo.Collection._publishCursor(query, self, "analyticsusers");
     return self.ready();
-
-  } else {
-    this.ready();
   }
+  this.ready();
 });

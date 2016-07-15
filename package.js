f6877b21e867ffd540e1ac6f00d78eeb5c3deec6
Charles Nelson <charlesnelson28@gmail.com>
2016-07-15 18:01:15 -0400

    Updated package.js for 1.3 imports syntax

--- package.js
+++ package.js
@@ -12,23 +12,23 @@ Npm.depends({
 
 Package.onUse(function (api) {
   api.versionsFrom("1.0.3.1");
-  api.use("mongo");
+
+  // "ecmascript" is mandatory dependency to compile our es6.
+  api.use("ecmascript");
+  api.use("es5-shim");
+
+  // weak dependencies indicate we will load after the following packages.
   api.use("accounts-base", ["client", "server"], { weak: true });
   api.use("browser-policy-content", "server", { weak: true });
 
-  // weak dependencies indicate we will load after the following packages
-  // and constrain their versions IF another package, or app brings them in
+  // constrain their versions IF another package, or app brings them in.
   api.use("iron:router@1.0.7", "client", { weak: true });
   api.use("meteorhacks:flow-router@1.17.2", "client", { weak: true });
   api.use("kadira:flow-router@2.6.0", "client", { weak: true });
 
-  api.addFiles([
-    "server/browser-policy.js",
-    "server/publications.js",
-  ], "server");
-  api.addFiles([
-    "vendor/analytics.min.js",
-    "client/collections.js",
-    "client/meteor-analytics.js",
-  ], "client");
+  api.mainModule("client-main.js", "client");
+  api.mainModule("server-main.js", "server");
+
+  // For backward compatibility.
+  api.export("analytics", ["client", "server"]);
 });

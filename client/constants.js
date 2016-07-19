c3ccf09024af4f39649def2b2b80fdc82891238b
Charles Nelson <charlesnelson28@gmail.com>
2016-07-19 12:48:07 -0400

    added false to SETTINGS

--- client/constants.js
+++ client/constants.js
@@ -1,6 +1,6 @@
 import { Meteor } from "meteor/meteor";
 
 const SETTINGS = Meteor.settings && Meteor.settings.public &&
-                 Meteor.settings.public.analyticsSettings;
+                 Meteor.settings.public.analyticsSettings || false;
 
 export { SETTINGS };

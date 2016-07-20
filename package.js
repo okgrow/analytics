4bc0675b6a3f4b9fcd0873b1a8ba48ceb7e4a6e0
Charles Nelson <charlesnelson28@gmail.com>
2016-07-19 20:54:47 -0400

    bumped versionFrom to 1.3 & added note

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("1.0.3.1");
+  api.versionsFrom("1.3.1");
 
   // "ecmascript" is mandatory dependency to compile our package's es6 code.
   api.use("ecmascript");
@@ -19,13 +19,20 @@ Package.onUse(function (api) {
   api.use("browser-policy-content", "server", { weak: true });
 
   // constrain their versions IF another package, or app brings them in.
+  // Below packages will cause constraint conflicts in Meteor Apps < 1.3.1
+  // Please use v1.0.9 https://github.com/okgrow/analytics/releases/tag/v1.0.9
+  // for Meteor Apps < 1.3.1
   api.use("iron:router@1.0.7", "client", { weak: true });
   api.use("meteorhacks:flow-router@1.17.2", "client", { weak: true });
   api.use("kadira:flow-router@2.6.0", "client", { weak: true });
 
-  api.mainModule("client-main.js", "client");
-  api.mainModule("server-main.js", "server");
-
+  try {
+    api.mainModule("client-main.js", "client");
+    api.mainModule("server-main.js", "server");
+  } catch (e) {
+    // We supress and ignore error for backwards compatibility for apps < 1.3.1,
+    // reference to issue here https://github.com/meteor/meteor/issues/6713
+  }
   // For backward compatibility.
   api.export("analytics", ["client", "server"]);
 });

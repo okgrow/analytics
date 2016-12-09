d8d3feeddd76aa6e5590bf2e506b893c564971d8
Paul Dowman <paul@pauldowman.com>
2016-12-09 13:04:41 -0500

    Refactor router-agnostic code in client/startup.js

--- client/startup.js
+++ client/startup.js
@@ -1,49 +1,66 @@
-import { Meteor } from "meteor/meteor";
-import { Tracker } from "meteor/tracker";
-import { SETTINGS } from "./constants";
-import { trackLogins, getUserEmail } from "./meteor-analytics";
-import { trackPageWhenReady } from "./helpers";
-import analytics from "../vendor/analytics.min.js";
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import { SETTINGS } from './constants';
+import { trackLogins, getUserEmail } from './meteor-analytics';
+import { trackPageWhenReady } from './helpers';
+import analytics from '../vendor/analytics.min';
+
+const logPageLoad = (title, referrer) => {
+  // Use setTimeout so it uses the location from after the route change
+  setTimeout(() => {
+    const page = {
+      title,
+      referrer,
+      path: location.pathname,
+      search: location.search,
+      url: location.href,
+    };
+
+    // Track page on analytics
+    trackPageWhenReady(page.title, page);
+  }, 0);
+};
+
+const logFirstPageLoad = () => {
+  logPageLoad(document.title, document.referrer);
+};
+
+const configurePageLoadTracking = () => {
+  const fireOnPushState = () => {
+    try {
+      logPageLoad({ title: document.title, referrer: location.href });
+    } catch (e) {
+      console.error(e); // eslint-disable-line no-console
+    }
+  };
+
+  // Save reference to original pushState.
+  const originalPushState = window.history.pushState;
+
+  // Wrap original pushState to call new push state function
+  // NOTE: this can't be an arrow function!
+  window.history.pushState = function okgrowAnalyticsMonkeyPatchedPushState(...args) {
+    fireOnPushState();
+
+    // Call original pushState with incoming arguments
+    return originalPushState.apply(window.history, args);
+  };
+
+  window.addEventListener('popstate', () => {
+    fireOnPushState();
+  }, false);
+};
 
 Meteor.startup(() => {
   if (SETTINGS) {
     analytics.initialize(SETTINGS);
-    
-    // Save reference to original pushState.
-    const pushState = window.history.pushState;
-    window.history.pushState = function(state) {
-      if (typeof window.history.onpushstate == "function") {
-        window.history.onpushstate({ title: document.title, referrer: location.href });
-      }
-      return pushState.apply(window.history, arguments);
-    }
-    
-    window.addEventListener('popstate', function() {
-      if (typeof window.history.onpushstate == "function") {
-        window.history.onpushstate({ title: document.title, referrer: location.href });
-      }
-    }, false);
-    
-    window.history.onpushstate = function({ title, referrer }) {
-      // Use defer so it doesnt use the previous location.
-      Meteor.defer(() => {
-        const page = {
-          title,
-          referrer,
-          path: location.pathname,
-          search: location.search,
-          url: location.href
-        };
-        // Track page on analytics
-        trackPageWhenReady(page.title, page);
-      });
-    }
-    
+    logFirstPageLoad();
+    configurePageLoadTracking();
   } else {
-    console.error("Missing analyticsSettings in Meteor.settings.public");
+    console.error('Missing analyticsSettings in Meteor.settings.public'); // eslint-disable-line no-console
   }
 
-  if (Package["accounts-base"]) {
+  if (Package['accounts-base']) {
     Tracker.autorun(() => {
       userEmail = getUserEmail();
     });
